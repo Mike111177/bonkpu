cccdd11c4b05a617e15870bd2c2e3052bbdd2452
Mike Davidson <1724399+Mike111177@users.noreply.github.com>
2025-05-13 17:44:44 -0400

    moved instructions to its own file

---
+++ scripts/instructions.py
@@ -0,0 +1,68 @@
+HLT = 1 << 23
+MHI = 1 << 22
+MLI = 1 << 21
+RI = 1 << 20
+RO = 1 << 19
+II = 1 << 18
+AI = 1 << 16
+AO = 1 << 15
+BI = 1 << 14
+BO = 1 << 13
+EO = 1 << 12
+SU = 1 << 11
+FI = 1 << 10
+CE = 1 << 9
+CHO = 1 << 8
+CLO = 1 << 7
+CHI = 1 << 6
+CLI = 1 << 5
+X = 1 << 4
+X = 1 << 3
+X = 1 << 2
+X = 1 << 1
+IE = 1 << 0
+
+FLAG_ZERO = 0b1
+FLAG_SIGN = 0b10
+FLAG_CARRY = 0b100
+zeroSet = lambda f: f & FLAG_ZERO
+signSet = lambda f: f & FLAG_SIGN
+carrySet = lambda f: f & FLAG_CARRY
+overflowSet = lambda f: bool(f & FLAG_SIGN) != bool(f & FLAG_CARRY)
+
+# Gets prepended to all instructions
+instructions_prefix = [MHI | CHI, MLI | CLI, RO | II | CE, CE]
+
+jumpi = [
+    MHI | CHO,
+    MLI | CLO,
+    RO | BI | CE,
+    MHI | CHO,
+    MLI | CLO,
+    CHI | RO | CE,
+    CLI | BO,
+]
+
+skip_jumpi = [CE, CE]
+
+instructions_table = [
+    ("NOP", []),
+    ("LDAi", [MHI | CHO, MLI | CLO, RO | AI | CE]),
+    ("ADDi", [MHI | CHO, MLI | CLO, RO | BI | CE, EO | AI | FI]),
+    ("JMPi", jumpi),
+    ("JEZi", lambda f: (jumpi if zeroSet(f) else skip_jumpi)),
+    ("JNZi", lambda f: (jumpi if not zeroSet(f) else skip_jumpi)),
+    ("JSi", lambda f: (jumpi if signSet(f) else skip_jumpi)),
+    ("JNSi", lambda f: (jumpi if not signSet(f) else skip_jumpi)),
+    ("JCi", lambda f: (jumpi if carrySet(f) else skip_jumpi)),
+    ("JNCi", lambda f: (jumpi if not carrySet(f) else skip_jumpi)),
+    ("JOi", lambda f: (jumpi if overflowSet(f) else skip_jumpi)),
+    ("JNOi", lambda f: (jumpi if overflowSet(f) else skip_jumpi)),
+    ("HLT", [HLT]),
+]
+
+def instruction_post_fn(instruction: list[int]):
+    for i in range(len(instruction) - 1, -1, -1):
+        if instruction[i]:
+            instruction[i] |= IE
+            break
---
+++ scripts/microcode_gen.py
@@ -0,0 +1,77 @@
+# Control signals
+import filecmp
+import os
+import re
+import shutil
+import struct
+import tempfile
+
+from instructions import instructions_table, instructions_prefix, instruction_post_fn
+
+DEFAULT_OUTPUT_FOLDER = os.path.join(os.path.dirname(__file__), "..", "bin")
+
+
+def print_instructions(table):
+    print(f" INS | V | OPCODE")
+    print("-------------------")
+    for idx, [ins, _] in enumerate(table):
+        op, varient = (
+            (ins[: m.start()], ins[m.start() :])
+            if (m := re.search(r"[a-z]", ins))
+            else (ins, "")
+        )
+        print(f"{op:>4} | {varient:1} | {idx:08b}")
+
+
+def write_instructions(
+    table,
+    prefix=[],
+    post_fn=lambda a: a,
+    output_folder=DEFAULT_OUTPUT_FOLDER,
+):
+    bin_names = ["A", "B", "C", "MASTER"]
+    output_files = [os.path.join(output_folder, f"{name}.bin") for name in bin_names]
+    os.makedirs(output_folder, exist_ok=True)
+    with tempfile.TemporaryDirectory() as tempDir:
+        temp_files = [os.path.join(tempDir, f"{name}.bin") for name in bin_names]
+        try:
+            bins = [open(file, "wb") for file in temp_files]
+            for ins_addr in range(2048):
+                opcode = ins_addr & 255
+                flags = ins_addr >> 8
+                base = table[opcode][1] if opcode < len(table) else []
+                ins = [*prefix, *(base if isinstance(base, list) else base(flags))]
+                ins.extend([0] * (16 - len(ins)))
+                post_fn(ins)
+
+                for step in range(16):
+                    bins[0].write(bytes([(ins[step] >> 16) & 0xFF]))
+                    bins[1].write(bytes([(ins[step] >> 8) & 0xFF]))
+                    bins[2].write(bytes([ins[step] & 0xFF]))
+                    bins[3].write(struct.pack(">I", ins[step]))
+                if len(ins) > 16:
+                    print(
+                        f"WARNING: Instruction {ins_addr} is too large ({len(ins)}). Truncated. Opcode: {opcode}. Flags: {flags:03b}"
+                    )
+        finally:
+            for bin in bins:
+                bin.close()
+        for bin, temp, out in zip(bin_names, temp_files, output_files):
+            if os.path.isfile(out):
+                if filecmp.cmp(temp, out):
+                    print(f"No changes in bin {bin}")
+                else:
+                    shutil.copyfile(temp, out)
+                    print(f"Changes in bin {bin}")
+            else:
+                print(f"New bin {bin}")
+                shutil.copyfile(temp, out)
+
+
+if __name__ == "__main__":
+    print_instructions(instructions_table)
+    write_instructions(
+        instructions_table,
+        prefix=instructions_prefix,
+        post_fn=instruction_post_fn,
+    )
